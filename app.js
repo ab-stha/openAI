c5ec0a77bf81bed6f63dcaf4e3f0a8403924c323
Abhiparth Shrestha <abhiparth@hotmail.co.uk>
2024-12-24 17:38:36 +0000

    paste in challenge from next video in Scrimba course

--- app.js
+++ app.js
@@ -26,6 +26,18 @@ const openAi = new OpenAI ({
  * our life just right.
  * **/
 
+/**
+ * Challenge:
+ * 1. Ask OpenAI to explain something complicated 
+ *    to you. For example Quantum Computing.
+ * 
+ * Prompt Engineering Stretch Goals
+ * - See if you can control the level of complexity of 
+ *   the generated content, for example is this for 
+ *   10-year-olds or college kids?
+ * - See if you can control the length of the output.
+ * **/ 
+
 const message = [
     {
         role : 'system',
